fe1f310c2dd17ef76b34853db8aceb3767746b1f
SpadeSeveren <jbolzweber@gmail.com>
2018-02-26 12:42:12 -0700

    small change to parse_csv

--- parse_csv.js
+++ parse_csv.js
@@ -1,11 +1,16 @@
 const csvFilePath = 'csv.csv';
 const csv = require('csvtojson');
 
+let res;
+
 csv()
     .fromFile(csvFilePath)
     .on('json', (jsonObj) => {
-        console.log(jsonObj);
+        // console.log(jsonObj);
+        res = jsonObj;
     })
     .on('done', (error) => {
         console.log('end')
-    })
+    });
+
+console.log(res);
