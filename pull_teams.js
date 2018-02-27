6bd2fe23d3aefbebc1eb2837f428ed43d9855637
Bolz-Weber <judah.bolz-weber@C02LW7YSFH00.servicemagic.com>
2018-02-27 11:25:50 -0700

    made things look more pretty

--- pull_teams.js
+++ pull_teams.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
-const request = require('request')
+const request = require('request');
 const cheerio = require('cheerio');
+const splitStr = require('./split_team_number.js');
 
 let event = process.env.EVENT;
 let url = `https://www.thebluealliance.com/event/${event}`;
@@ -11,16 +12,20 @@ request(url, function (err, resp, body) {
     $ = cheerio.load(body);
     links = $('[href^="/team/"]'); //use your CSS selector here
     $(links).each(function (i, link) {
-        tempArr.push($(link).text() + ':\n  ' + $(link).attr('href'));
+        tempArr.push($(link).text());
     });
     tempArr.forEach(elem => {
-        let res = elem.slice(0, elem.indexOf(':'));
-        arr.push(res);
+        arr.push(elem);
         // console.log(res);
     });
 
     // console.log(arr);
-    arr.forEach(team => {
+    arr.forEach(teamDumb => {
+        let team = splitStr.split(teamDumb);
+        
+        if(!fs.existsSync('teams/')){
+            fs.mkdirSync('teams/');
+        }
         if (!fs.existsSync(`teams/${event}/`)) {
             fs.mkdirSync(`teams/${event}/`);
         }
---
+++ split_team_number.js
@@ -0,0 +1,34 @@
+module.exports = {
+    split: function (str) {
+        let arr = str.split('');
+        let res;
+        
+        for(let i = 0; i < arr.length; i++){
+                arr[i] = isNormalInteger(arr[i]) ? parseInt(arr[i]) : arr[i];
+        }
+
+        if (typeof (arr[1]) === 'string') {
+            res = `${arr[0]} ${str.slice(1)}`;
+        }
+        else if (typeof (arr[2]) === 'string') {
+            res = `${arr[0]}${arr[1]} ${str.slice(2)}`;
+        }
+        else if (typeof (arr[3]) === 'string') {
+            res = `${arr[0]}${arr[1]}${arr[2]} ${str.slice(3)}`;
+        }
+        else if (typeof (arr[4]) === 'string') {
+            res = `${arr[0]}${arr[1]}${arr[2]}${arr[3]} ${str.slice(4)}`;
+        }
+
+        //this one is just a precaution
+        else if (typeof (arr[5]) === 'string') {
+            res = `${arr[0]}${arr[1]}${arr[2]}${arr[3]}${arr[4]} ${str.slice(5)}`;
+        }
+        return(res);
+    }
+};
+
+function isNormalInteger(str) {
+    var n = Math.floor(Number(str));
+    return n !== Infinity && String(n) === str && n >= 0;
+}
