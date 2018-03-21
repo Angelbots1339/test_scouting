dec5fbae3df106892496b2453eba6ae0df1433a6
SpadeSeveren <jbolzweber@gmail.com>
2018-03-21 13:54:06 -0600

    yup

--- ui/pug_to_html.js
+++ ui/pug_to_html.js
@@ -5,8 +5,9 @@ let event = process.env.EVENT;
 let teamdir = fs.readdirSync(`./teams/${event}`);
 
 let html = pug.renderFile('ui/home.pug', {
+    event: event,
     teamDir: teamdir,
     name: 'TEAMS'
 });
 
-fs.writeFileSync('ui/home.html', html, 'utf8');
+fs.writeFileSync(`ui/${event}/home.html`, html, 'utf8');
