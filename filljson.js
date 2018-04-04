f764a726e01f22b5ec1dc66ae9949c049189dfe1
SpadeSeveren <jbolzweber@gmail.com>
2018-04-04 10:35:13 -0600

    added averager route

--- filljson.js
+++ filljson.js
@@ -1,28 +1,33 @@
 const fs = require('fs');
 const script = require('./average.js');
 
-let newMatches = [];
-let event;
+module.exports = {
 
-let newData = fs.readdirSync('../new_data');
-newData.forEach(match => {
-    let data = require(`../new_data/${match}`);
-    let team = data.teamName;
-    event = data.eventName;
-    let stats = data;
-    let matchNum = data.match;
-    let json = JSON.stringify(stats);
-    fs.writeFileSync(`./teams/${event}/${team}/matches/${match}.json`, json, 'utf8');
-    if (newMatches.indexOf(team) === -1) {
-        newMatches.push(team);
+    fill: function () {
+        let newMatches = [];
+        let event;
+
+        let newData = fs.readdirSync('../new_data');
+        newData.forEach(match => {
+            let data = require(`../new_data/${match}`);
+            let team = data.teamName;
+            event = data.eventName;
+            let stats = data;
+            let matchNum = data.match;
+            let json = JSON.stringify(stats);
+            fs.writeFileSync(`./teams/${event}/${team}/matches/${match}.json`, json, 'utf8');
+            if (newMatches.indexOf(team) === -1) {
+                newMatches.push(team);
+            }
+        });
+
+        newMatches.forEach(team => {
+            console.log('THERE');
+            script.average(team, event);
+        });
+
+        newData.forEach(file => {
+            fs.unlinkSync(`../new_data/${file}`);
+        });
     }
-});
-
-newMatches.forEach(team => {
-    console.log('THERE');
-    script.average(team, event);
-});
-
-newData.forEach(file => {
-    fs.unlinkSync(`../new_data/${file}`);
-});
+};
--- ui/hub/routes/index.js
+++ ui/hub/routes/index.js
@@ -9,21 +9,26 @@ const jsonParser = bodyParser.json();
 const urlEncodedParser = bodyParser.urlencoded({ extended: false });
 const winston = require('winston');
 const path = require('path');
+const fill = require('../../../filljson.js');
 winston.level = process.env.LOG || 'info';
 
-router.get('/', function(req, res) {
+router.get('/', function (req, res) {
     res.sendFile(path.resolve('ui/hub/views/index.html'));
 });
 
 
-router.get('/Colorado/:team', function(req, res) {
-    let data = require(`../../../teams/colorado/${req.params.team}/average.json`);
+router.get('/:event/:team', function (req, res) {
+    let data = require(`../../../teams/${req.params.event}/${req.params.team}/average.json`);
     let teamData = {
         averageCubes: data.cubes,
         averageDriving: data.driving,
         averageAuto: data.auto
     };
     return res.status(200).send(teamData);
+});
+
+router.get('/dothething', function (req, res) {
+    fill.fill();
 });
 
 module.exports = router;
